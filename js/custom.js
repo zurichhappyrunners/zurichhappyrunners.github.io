71ef19697458546ab639889850088cda7d102901
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-11-19 20:40:20 +0100

    - Publishing web

--- js/custom.js
+++ js/custom.js
@@ -9,23 +9,23 @@
 
 
 	//jQuery to collapse the navbar on scroll
-	$(window).scroll(function() {
-		if ($(".navbar").offset().top > 50) {
-			$(".navbar-fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".navbar-fixed-top").removeClass("top-nav-collapse");
-		}
-	});
+	// $(window).scroll(function() {
+	// 	if ($(".navbar").offset().top > 50) {
+	// 		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	// 	} else {
+	// 		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	// 	}
+	// });
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
-		$('.navbar-nav li a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
+		// $('.navbar-nav li a').bind('click', function(event) {
+		// 	var $anchor = $(this);
+		// 	$('html, body').stop().animate({
+		// 		scrollTop: $($anchor.attr('href')).offset().top
+		// 	}, 1500, 'easeInOutExpo');
+		// 	event.preventDefault();
+		// });
 		$('.page-scroll a').bind('click', function(event) {
 			var $anchor = $(this);
 			$('html, body').stop().animate({
