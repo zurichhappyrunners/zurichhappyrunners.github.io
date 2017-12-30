e83a0c3a98b73bb791fe5423ddcb323f8bf133df
Jordi Pont-Tuset <jponttuset@gmail.com>
2017-12-30 21:01:08 +0100

    - Different years added

--- csv_data/parse.py
+++ csv_data/parse.py
@@ -1,104 +1,107 @@
 import csv
 import time
-import sys, os
+import sys
+import os
 import re
 
 
+def lut_members_from_csv(csv_file):
+    with open(csv_file, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+
+        # Get the column IDs we're interested in
+        field_ids = []
+        firstrow = csvreader.next()
+        for field in sel_fields:
+            field_ids.append(firstrow.index(field))
+
+        # Create LUT
+        currmembers = dict()
+        for row in csvreader:
+            if len(row) > 0:
+                currmembers[row[field_ids[1]]] = [row[field_ids[0]], int(float(row[field_ids[2]]))]
+
+    return currmembers
+
+
+def print_to_file(type, date, members, ids):
+    with open(csv_folder + 'Ranking-'+type+'-'+date+'.csv', 'w') as csvfile:
+        csvfile.write('"Name","Meetups attended"\n')
+        for id in ids:
+            if id[0] > 0:
+                csvfile.write('"%s",%d\n' % (members[id[1]][0], id[0]))
+
+
 # Parameters
-if (len(sys.argv)>1):
-    curr_date = sys.argv[1];
+if len(sys.argv) > 1:
+    curr_date = sys.argv[1]
 else:
-    curr_date = time.strftime("%m-%d-%y");
+    curr_date = time.strftime("%m-%d-%y")
 
+# Global variables
 download_folder = '/Users/jpont/Downloads/'
-csv_folder = '/Users/jpont/Workspace/zurichhappyrunners/csv_data/'
-sel_fields = ["Name", "Member ID", "Meetups attended"]
-html_file = os.path.dirname(os.path.realpath(__file__))+"/../index.html"
+root_folder = '/Users/jpont/Workspace/zurichhappyrunners/'
+csv_folder = root_folder + 'csv_data/'
+sel_fields = ['Name', 'Member ID', 'Meetups attended']
+html_file = root_folder + '/index.html'
 
-## Move the file
-os.rename(download_folder+"Zurich-Happy-Runners_Member_List_on_"+curr_date+".xls", csv_folder+"Zurich-Happy-Runners_Member_List_on_"+curr_date+".xls")
+# Move the file to csv folder
+# os.rename(download_folder+"Zurich-Happy-Runners_Member_List_on_"+curr_date+".xls", csv_folder+"Zurich-Happy-Runners_Member_List_on_"+curr_date+".xls")
 
-## Change the HTML
+# ******* Update the HTML *******
+# Read the HTML
 with open(html_file) as infile:
     html_str = infile.read()
 
+# Look for the old date
 match = re.search(r'\d{2}-\d{2}-\d{2}', html_str)
 old_date = str(match.group())
 
+# Replace the date string
 html_str = html_str.replace(old_date, curr_date)
 
+# Write back to HTML file
 print "Updating ranking from " + old_date + " to " + curr_date
 with open(html_file, 'w') as outfile:
     outfile.write(html_str)
-        
-         
-## ******** Get the global ranking ********
-# Open the current file
-with open(csv_folder + 'Zurich-Happy-Runners_Member_List_on_'+curr_date+'.xls', 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter='\t')
 
-    # Get the column IDs we're interested in
-    field_ids = []
-    firstrow = csvreader.next()
-    for field in sel_fields:
-        field_ids.append(firstrow.index(field))
-        
-    # Create LUT
-    currmembers = dict()
-    for row in csvreader:
-        if len(row)>0:
-            currmembers[row[field_ids[1]]] = [row[field_ids[0]],  int(float(row[field_ids[2]]))]
+# ******* Get the global ranking *******
+# Read the current file and create a LUT w.r.t. member ID
+currmembers = lut_members_from_csv(csv_folder + 'Zurich-Happy-Runners_Member_List_on_' + curr_date + '.xls')
     
-# Get the current ranking
+# Sort ids
 to_sort = []
 for mem in currmembers:
     to_sort.append([currmembers[mem][1], mem])
 to_sort.sort(reverse=True)
     
-# Print ranking to file
-with open(csv_folder + 'Ranking-Global-'+curr_date+'.csv', 'w') as csvfile:
-    csvfile.write('"Name","Meetups attended"\n')
-    for id in to_sort:
-        if currmembers[id[1]][1]>0:
-            csvfile.write('"%s",%d\n'%(currmembers[id[1]][0],currmembers[id[1]][1]))
+# Print to file
+print_to_file('Global', curr_date, currmembers, to_sort)
 
 # Display
-print 'We are currently %d members.'%len(currmembers)
-        
-        
-## ******** Get the year ranking ********
-# Open the file as of last day of previous year
-with open(csv_folder + 'Zurich-Happy-Runners_Member_List_on_12-31-16.xls', 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter='\t')
-    
-    # Get the column IDs we're interested in
-    field_ids = []
-    firstrow = csvreader.next()
-    for field in sel_fields:
-        field_ids.append(firstrow.index(field))
-    
-    # Create LUT
-    members_prev_year = dict()
-    for row in csvreader:
-        if len(row)>0:
-            members_prev_year[row[field_ids[1]]] = [row[field_ids[0]], int(float(row[field_ids[2]]))]
+print 'We are %d members currently.' % len(currmembers)
 
-# Get the year ranking
-to_sort = []
-for mem in currmembers:
-    # Was him/her a member last year?
-    if mem in members_prev_year:
-        to_sort.append([currmembers[mem][1]-members_prev_year[mem][1], mem])
-    else:
-        to_sort.append([currmembers[mem][1], mem])
-to_sort.sort(reverse=True)
-    
-# Print ranking to file
-with open(csv_folder + 'Ranking-Year-'+curr_date+'.csv', 'w') as csvfile:
-    csvfile.write('"Name","Meetups attended"\n')
-    for id in to_sort:
-        if id[0]>0:
-            csvfile.write('"%s",%d\n'%(currmembers[id[1]][0],id[0]))
-        
-# Display
-print 'There were %d members at the end of 2016.'%len(members_prev_year)
+# ******** Get the years ranking ********
+years = {'2017': {'last-before': '12-31-16', 'last-updated': '12-30-17'},
+         '2018': {'last-before': '12-30-17', 'last-updated': curr_date}}
+
+for year in years:
+    # Open the file as of last day of previous year
+    members_prev_year = lut_members_from_csv(csv_folder + 'Zurich-Happy-Runners_Member_List_on_' + years[year]['last-before'] + '.xls')
+
+    # Get the year ranking
+    to_sort = []
+    for mem in currmembers:
+        # Was him/her a member last year?
+        if mem in members_prev_year:
+            if currmembers[mem][1] > members_prev_year[mem][1]:
+                to_sort.append([currmembers[mem][1]-members_prev_year[mem][1], mem])
+        else:
+            to_sort.append([currmembers[mem][1], mem])
+    to_sort.sort(reverse=True)
+
+    # Print ranking to file
+    print_to_file(year, years[year]['last-updated'], currmembers, to_sort)
+
+    # Display
+    print 'There were %d members active during %s.' % (len(members_prev_year), year)
